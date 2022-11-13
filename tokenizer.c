3fb746c92b432c62d427fe55b945378ff761dab4
Jesse <ibbraidejesse@gmail.com>
2022-11-13 19:39:43 +0100

    updated tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -7,10 +7,7 @@ char **tokenizer(char *cmd)
     int argc = 0, i = 0;
     struct stat st;
 
-	cmdcpy = malloc(sizeof(char) * strlen(cmd));
-
-	strcpy(cmdcpy, cmd);
-
+	cmdcpy = strdup(cmd);
 	token = strtok(cmd, " \n");
 
 	/* get number of arguments ==> argc */
@@ -22,11 +19,8 @@ char **tokenizer(char *cmd)
 	/* insert strings in string array */
 	token = strtok(cmdcpy, " \n");
 	while (token)
-	{
-		argv[i] = malloc(sizeof(char) * strlen(token));
-		strcpy(argv[i++], token);
-		token = strtok(NULL, " \n");
-	}
+        argv[i++] = token, token = strtok(NULL, " \n");
+
 	argv[i] = NULL;
     if (argv[0][0] == '/' && stat(cmd, &st) == 0)
         exec(argv);
